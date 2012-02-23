66e4791ae1c8d86efd9df414ce837b338b194439
Sami Kanza <samikanza@gmail.com>
2012-02-23 19:56:48 +0000

    Updated to version 0.1-3
    Updated index file to point to correct feeds.conf file
    Updated man page to detail how to run software in the command line
    Reverted control file to be standards compliant
    Updated changelog to reflect these changes

--- info6005-cw1-sk11g08/index.php
+++ info6005-cw1-sk11g08/index.php
@@ -46,7 +46,7 @@
 
 	# Read config from file
 	if (@!$config) {
-		$config_file = 'feeds.conf';
+		$config_file = '/etc/info6005-cw1-sk11g08/feeds.conf';
 		if (file_exists($config_file)) {
 			$config = conf_from_file($config_file);
 		}
